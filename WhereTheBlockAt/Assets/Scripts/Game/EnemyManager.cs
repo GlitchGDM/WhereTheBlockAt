e6b5b5661489800c1736a854c1fd255887a7e342
Guillermo F. Dezcallar <guilledezcallar@gmail.com>
2019-01-31 11:35:57 +0100

    Cerca de terminar. Falta: meter timer, que se destruyan los aldeanos, ver si puedo hacer algo con lo de que haya uno en escena, ver si puedo instanciar el enemymanagaer o el gamemanager, arreglar menu arreglar loading screen

---
+++ WhereTheBlockAt/Assets/Scripts/Game/EnemyManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : TemporalSingleton<EnemyManager> {
+
+	#region Variables
+
+	public GameObject enemy;
+
+	public int numberEnems = 15;
+
+	private List<GameObject> enemyVector = new List<GameObject>();
+
+	public Material[] mats;
+	#endregion
+
+	#region Unity Methods
+
+	private void Start()
+	{
+		CreateEnemies(numberEnems);
+	}
+
+	public void CreateEnemies(int numberOfEnemies)
+	{
+
+		for(int i = 0; i < numberOfEnemies; i++)
+		{
+			GameObject enemyAux = Instantiate(enemy, Vector3.zero, Quaternion.identity);
+			enemyAux.SetActive(false);
+			enemyVector.Add(enemyAux);
+			SpawnEnemy();
+		}
+		SetVillains();
+	}
+
+	void SpawnEnemy()
+	{
+		for (int i = 0; i < enemyVector.Count; i++)
+		{
+			if (!enemyVector[i].activeSelf)
+			{
+				enemyVector[i].transform.position = new Vector3(Random.Range(enemyVector[i].GetComponentInChildren<VillagerAI>().minPosWorld.position.x, enemyVector[i].GetComponentInChildren<VillagerAI>().maxPosWorld.position.x),0, Random.Range(enemyVector[i].GetComponentInChildren<VillagerAI>().minPosWorld.position.z, enemyVector[i].GetComponentInChildren<VillagerAI>().maxPosWorld.position.z));
+				enemyVector[i].SetActive(true);
+				enemyVector[i].GetComponentInChildren<MeshRenderer>().material = mats[i];
+				break;
+			}
+		}
+	}
+
+	void SetVillains()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			enemyVector[i].GetComponentInChildren<VillagerAI>().isTargetToKill = true;
+		}
+	}
+
+	#endregion
+}
---
+++ WhereTheBlockAt/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	#region Variables
+
+	public int lives = 3;
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start () 
+	{
+		MusicManager.Instance.PlayBackgroundMusic(AppMusic.Game_Music);
+		Cursor.visible = false;
+	}
+	
+	void Update () 
+	{
+		if (lives <= 0)
+		{
+			Cursor.visible = true;
+			SceneManager.LoadScene(AppScenes.Menu_Scene);
+		}
+	}
+
+	#endregion
+}
